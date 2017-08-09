628ab12869a242713af2591758dc519bb8ca5d49
wangtianjing <wangtianjing@bbdservice.com>
2017-08-09 10:31:59 +0800

    虚拟币交易管理

---
+++ web/src/main/java/com/jubi/service/CoinOrderService.java
@@ -0,0 +1,60 @@
+/**
+ * BBD Service Inc
+ * All Rights Reserved @2016
+ */
+package com.jubi.service;
+
+/**
+ * 虚拟币交易服务
+ *
+ * @author tjwang
+ * @version $Id: CoinOrderService.java, v 0.1 2017/8/9 0009 9:59 tjwang Exp $
+ */
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.jubi.dao.CoinOrderDao;
+import com.jubi.dao.entity.CoinOrderEntity;
+import com.jubi.dao.entity.CoinOrderEntityExample;
+import com.jubi.service.vo.CoinOrderVo;
+import com.jubi.util.BeanMapperUtil;
+import com.jubi.util.DateUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class CoinOrderService {
+
+    @Autowired
+    private CoinOrderDao coinOrderDao;
+
+    /**
+     * 获取当天所有交易信息
+     *
+     * @param coin
+     * @return
+     */
+    public List<CoinOrderVo> queryCurrentDayOrders(String coin) {
+        Preconditions.checkArgument(StringUtils.isNotBlank(coin), "虚拟币不能为空");
+
+        List<CoinOrderVo> result = Lists.newArrayList();
+
+        int beginTime = DateUtils.getCurrentDayBeginTime();
+        CoinOrderEntityExample exam = new CoinOrderEntityExample();
+        exam.createCriteria().andTradeTimeGreaterThanOrEqualTo(beginTime);
+
+        List<CoinOrderEntity> ds = coinOrderDao.selectByExample(exam);
+        for (CoinOrderEntity d : ds) {
+            CoinOrderVo vo = BeanMapperUtil.map(d, CoinOrderVo.class);
+            vo.setTradeTime(new Date(d.getTradeTime() * 1000));
+            result.add(vo);
+        }
+
+        return result;
+    }
+
+}
---
+++ web/src/main/java/com/jubi/service/vo/CoinOrderVo.java
@@ -0,0 +1,64 @@
+/**
+ * BBD Service Inc
+ * All Rights Reserved @2016
+ */
+package com.jubi.service.vo;
+
+import java.util.Date;
+
+/**
+ * @author tjwang
+ * @version $Id: CoinOrderVo.java, v 0.1 2017/8/9 0009 10:06 tjwang Exp $
+ */
+public class CoinOrderVo {
+
+    private Integer tid;
+
+    private String coin;
+
+    private Double price;
+
+    private Double amount;
+
+    private Date tradeTime;
+
+    public Integer getTid() {
+        return tid;
+    }
+
+    public void setTid(Integer tid) {
+        this.tid = tid;
+    }
+
+    public String getCoin() {
+        return coin;
+    }
+
+    public void setCoin(String coin) {
+        this.coin = coin;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Date getTradeTime() {
+        return tradeTime;
+    }
+
+    public void setTradeTime(Date tradeTime) {
+        this.tradeTime = tradeTime;
+    }
+}
